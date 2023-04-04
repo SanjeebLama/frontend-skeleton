1bd9755c5efca83273c6c0d46a383bb92aec17a4
Sanjeeb Lama <51410633+SanjeebLama@users.noreply.github.com>
2023-04-04 20:45:32 +0545

    Add Shared Config for next-i18next

---
+++ packages/next-i18next-config/next-i18next.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'ja'],
+  },
+  fallbackLng: {
+    default: ['en'],
+  },
+  localePath:require('path').resolve('./public/static/locales')
+}
